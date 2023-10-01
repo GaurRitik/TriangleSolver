2ec08247e0e9808e67cc5d560533fc55e4796151
Ritik Gaur <80382265+GaurRitik@users.noreply.github.com>
2023-10-02 03:24:23 +0530

    added the isosceles triangle test cases

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -30,5 +30,59 @@ namespace TestClass
                 Assert.AreEqual(expected, actual);
             }
         }
+         public class ValidIsoscelesTriangleTest
+ {
+     [Test]
+     public void AnalyzeTriangle_Input9and9and7_OutputIsoscelestriangle()
+     {
+         //Arrange
+         int firstSide = 9;
+         int secondSide = 9;
+         int thirdSide = 7;
+
+         string expected = "Isosceles triangle";
+
+         //Act
+         string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+         //Assert
+         Assert.AreEqual(expected, actual);
+     }
+
+     [Test]
+     public void AnalyzeTriangle_Input9and7and9_OutputIsoscelestriangle()
+     {
+         //Arrange
+         int firstSide= 9;
+         int secondSide= 7;
+         int thirdSide= 9;
+
+         string expected = "Isosceles triangle";
+
+         //Act
+         string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+         //Assert
+         Assert.AreEqual(expected, actual);
+     }
+
+     [Test]
+     public void AnalyzeTriangle_Input7and9and9_OutputIsoscelestriangle()
+     {
+
+         //Arrange
+         int firstSide = 7;
+         int secondSide = 9;
+         int thirdSide = 9;
+
+         string expected = "Isosceles triangle";
+
+         //Act
+         string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+         //Assert
+         Assert.AreEqual(expected, actual);
+     }
+ }
     }
 }
